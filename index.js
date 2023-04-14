565a43496c2643581f041d4075469e2454d13395
Khuzema <maslam002@citymail.cuny.edu>
2023-04-14 04:21:15 -0400

    Redid from scratch. Added circle.

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions.js');
+const fileName = "./logo.svg";
+const setShape = require('./lib/setShape.js')
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    }
-  ])
-  .then((answers) => {
-    console.log(`Hello, ${answers.name}!`);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// function to create new svg file using inquirer response and file system
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
+}
+
+// initialize, ask questions then createLogo using responses, catch any errors
+function init() {
+    inquirer 
+    .prompt(questions)
+    .then((response) => {
+        createLogo(response);
+        })
+    .catch(err => {
+            console.log(err)
+        });
+}
+
+init();
